f9e554b91c2a50a7b7c1c227b1e4f5f35a4d7f38
Piyush <apsonline21112003@gmail.com>
2025-03-28 08:01:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -291,20 +291,7 @@ function toggleMenu() {
 
 
 // Profile Page Data Fetch
-/*firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-        // User logged in, show details
-        document.getElementById("user-name").innerText = user.displayName || "No Name";
-        document.getElementById("user-email").innerText = user.email || "No Email";
-        document.getElementById("profile-img").src = user.photoURL || "default-avatar.png";
-        document.getElementById("user-uid").innerText = user.uid;
-        document.getElementById("last-login").innerText = user.metadata.lastSignInTime;
-    } else {
-        // User not logged in, redirect to index.html
-        console.log("Please login");
-        window.location.href = "index.html";  // Redirect to home page
-    }
-});*/
+
 document.addEventListener("DOMContentLoaded", function() {
     firebase.auth().onAuthStateChanged((user) => {
         if (user) {
