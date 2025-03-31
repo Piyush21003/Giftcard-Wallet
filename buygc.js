556a4bde734bb82bb7807232b3532e8833162940
Piyush <apsonline21112003@gmail.com>
2025-03-31 18:55:22 +0530

    Create buygc.js

---
+++ buygc.js
@@ -0,0 +1,9 @@
+//navbar
+function toggleMenu() {
+            var menu = document.getElementById("mobileMenu");
+            if (menu.style.display === "block") {
+                menu.style.display = "none";
+            } else {
+                menu.style.display = "block";
+            }
+        }
